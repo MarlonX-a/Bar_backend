5159e6430e590385ba0ac308b952506414fd1021
MarlonX-a <marlonalvime05@gmail.com>
2026-08-08 16:22:04 -0500

    pequeña corrección

--- bar_api/src/rols/rols.module.ts
+++ bar_api/src/rols/rols.module.ts
@@ -4,9 +4,10 @@ import { RolsController } from './rols.controller';
 import { Rol } from './entities/rol.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PermissionsGuard } from '../auth/authorization/permissions.guard';
+import { Permission } from './entities/permission.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Rol])],
+  imports: [TypeOrmModule.forFeature([Rol, Permission])],
   controllers: [RolsController],
   providers: [RolsService, PermissionsGuard],
   exports: [RolsService],
